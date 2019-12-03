bab585b6a25c83819f1575605322df05919bdf6f
vesh95 <vesh95.17@ya.ru>
2019-12-03 18:42:08 +0500

    Add Hand class

---
+++ lib/hand.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative 'card'
+
+class Hand
+  def initialize
+    @cards = []
+  end
+
+  def <<(card)
+    if card.value == 'A'
+      @cards << card
+    else
+      @cards.unshift(card)
+    end
+    @cards
+  end
+
+  def show
+    @cards.map(&:show).join('')
+  end
+
+  def score
+    @cards.inject(0) do |sum, card|
+      t = if %w[J Q K].include?(card.value)
+            10
+          elsif card.value == 'A'
+            sum + 11 >= 21 ? 1 : 11
+          else
+            card.value
+          end
+      sum + t
+    end
+  end
+end
