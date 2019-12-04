ca02e98cb37aed3ee538c5621ae0ee3d0666c088
vesh95 <vesh95.17@ya.ru>
2019-12-04 12:34:27 +0500

    Implements greeting, actions_list dialogs

--- lib/game/dialogs.rb
+++ lib/game/dialogs.rb
@@ -4,6 +4,12 @@ class Game
 
   private
 
+  def greeting!
+    puts('Как вас зовут?')
+    nick = gets.chomp.to_s
+    @player = User.new(nick)
+  end
+
   def summary(player)
     puts("#{player.name} $#{player.bank}")
     puts("#{player.score} #{player.shadow_cards}") # FIX: shadow_score
@@ -19,6 +25,12 @@ class Game
   def table_summary(results = :close)
     puts("Банк #{@bank}".rjust(30, '#'))
     @players.each { |p| results == :open ? open_summary(p) : summary(p) }
+  end
+
+  def actions_list(player)
+    puts('p - пропустить ход')
+    puts('a - добавить карту') if player.cards_count < 3
+    puts('o - открыть карты')
   end
 
 end
